4a3003dce155c30b2705d31d31ed7c862c0251f7
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-07 18:52:12 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,9 +1,10 @@
 CONFIG = {                                                                                                                                                              
-    # 'mode': 'wsgi',                                                                                                                                                   
-    'working_dir': '/path/to/my/app',                                                                                                                                   
-    # 'python': '/usr/bin/python',                                                                                                                                      
+    'mode': 'wsgi',                                                                                                                                                   
+    'working_dir': '/home/box/web/',                                                                                                                                   
+    'python': '/usr/bin/python', 
+    #pythonpath="path"
     'args': (                                                                                                                                                           
-        '--bind=127.0.0.1:8000',                                                                                                                                      
+        '--bind=0.0.0.0:8080',                                                                                                                                      
         '--workers=16',                                                                                                                                                 
         '--timeout=60',                                                                                                                                                 
         'app.module',                                                                                                                                                   
