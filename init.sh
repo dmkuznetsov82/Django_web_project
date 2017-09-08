55697c838af44206ef571fa6694b2c5a5d822a3a
dmkuznetsov82 <dmkuznetsov82@gmail.com>
2017-09-08 20:18:32 +0000

    modified:   etc/nginx.conf 	modified:   init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 #sudo ln -fs /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo ln -fs /home/box/web/etc/gunicorn_2.conf /etc/gunicorn.d/test_2
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:application &
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:application &
 sudo gunicorn -c /home/box/web/etc/gunicorn_2.conf ask.ask.wsgi:application &
 #sudo /etc/init.d/gunicorn restart test
 #sudo /etc/init.d/gunicorn restart test_2
