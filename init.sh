cf1a9b7d44ffa4a931be2daccee6a946fe446c8e
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-08 10:21:46 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -fs /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 sudo ln -fs /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/gunicorn restart test
 #sudo /etc/init.d/mysql start
