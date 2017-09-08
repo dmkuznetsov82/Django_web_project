0c93d299749e80d6af6ab63dc369e1b80c89d652
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-08 12:21:36 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ def application(env, start_response):
     params = urlparse.parse_qs(env['QUERY_STRING'])
     body = ""
     for key,value in params.items():
-        body = body + str(key)+"="+str(value)
+        body = body + str(key)+"="+''.join(value)
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [body]   
