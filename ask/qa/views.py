ea96b4aa1b090f770c942a2d925c8209769f04d0
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-11 17:53:41 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,4 +3,4 @@
 # Create your views here.
 from django.http import HttpResponse
 def test (request,*args,**kwargs): 
-    return HttpResponse(content='OK',status=200)
+    return HttpResponse(content='OK')
