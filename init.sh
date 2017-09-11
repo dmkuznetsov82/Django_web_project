55ccb13e675fe81a80f4eac739d8976aa7504b56
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-11 18:43:40 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,10 +10,4 @@ sudo ln -fs /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -fs /home/box/web/etc/django.conf /etc/gunicorn.d/django
 sudo /etc/init.d/gunicorn restart
 
-#sudo ln -fs /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo ln -fs /home/box/web/etc/gunicorn_2.conf /etc/gunicorn.d/test_2
-#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:application &
-#sudo gunicorn -c /home/box/web/etc/gunicorn_2.conf ask.ask.wsgi:application &
-#sudo /etc/init.d/gunicorn restart test
-#sudo /etc/init.d/gunicorn restart test_2
 #sudo /etc/init.d/mysql start
