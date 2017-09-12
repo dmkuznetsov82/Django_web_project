ebe0a8c16da8888fba7dd1bd86589fac52787e80
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-12 22:44:15 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,36 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+#Question - вопрос
+#title - заголовок вопроса
+#text - полный текст вопроса
+#added_at - дата добавления вопроса
+#rating - рейтинг вопроса (число)
+#author - автор вопроса
+#likes - список пользователей, поставивших "лайк"
+class Question(models.model):
+  title = models.CharField(max_length=255)
+  text = models.TextField()
+  added_at = models.DateTimeField()
+  rating = models.IntegerField()
+  author = models.CharField(max_length=255)
+  likes = models.TextField()
+#Answer - ответ
+#text - текст ответа
+#added_at - дата добавления ответа
+#question - вопрос, к которому относится ответ
+#author - автор ответа
+class Answer(models.model):
+  text = models.TextField()
+  added_at = models.DateTimeField()
+  question = models.TextField()
+  author = models.CharField(max_length=255)
+  
+  class User(models.User): pass
+  
+#QuestionManager - менеджер модели Question
+#new - метод возвращающий последние добавленные вопросы
+#popular - метод возвращающий вопросы отсортированные по рейтингу
+class QuestionManager(object):
+  def new: pass
+  def popular: pass
