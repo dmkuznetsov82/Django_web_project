f600236d35e22450ccd60ec3e51d845eed817951
dmkuznetsov82 <dmkuznetsov82@gmail.com>
2017-09-13 18:29:29 +0000

    modified:   ask/ask/settings.py 	modified:   ask/qa/models.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'qa.models.Question',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,26 +1,15 @@
 from __future__ import unicode_literals
 from django.db import models
 
-#Question - вопрос
-#title - заголовок вопроса
-#text - полный текст вопроса
-#added_at - дата добавления вопроса
-#rating - рейтинг вопроса (число)
-#author - автор вопроса
-#likes - список пользователей, поставивших "лайк"
-class Question(models.model):
+class Question(models.Model):
   title = models.CharField(max_length=255)
   text = models.TextField()
   added_at = models.DateTimeField()
   rating = models.IntegerField()
   author = models.CharField(max_length=255)
   likes = models.TextField()
-#Answer - ответ
-#text - текст ответа
-#added_at - дата добавления ответа
-#question - вопрос, к которому относится ответ
-#author - автор ответа
-class Answer(models.model):
+
+class Answer(models.Model):
   text = models.TextField()
   added_at = models.DateTimeField()
   question = models.TextField()
@@ -28,9 +17,6 @@ class Answer(models.model):
   
   class User(models.User): pass
   
-#QuestionManager - менеджер модели Question
-#new - метод возвращающий последние добавленные вопросы
-#popular - метод возвращающий вопросы отсортированные по рейтингу
 class QuestionManager(models.Manager):                                          
         def new(self):                                                              
                 return self.order_by('-added_at')                                                          
