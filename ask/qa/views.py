c6e1f4c816a9eae7a626ac665312aa3e3b93bad0
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-18 12:06:04 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.decorators.http import require_GET
+
 def test (request,*args,**kwargs): 
     return HttpResponse('OK')
+
 def new (request,*args,**kwargs): 
     return HttpResponse('OK')
+
 def popular (request,*args,**kwargs): 
     return HttpResponse('OK')
-def question (request,*args,**kwargs): 
-    return HttpResponse('OK')
+
+@reguire_GET
+def question_details (request,id): 
+    question = get_object_or_404(Question, id=id)
+    return render(request, '/question/question_details.html','question':question)
