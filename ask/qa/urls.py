44562e117eb1becbaffddd04cd945d3d457641e5
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-18 11:57:52 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from qa.views import test
 urlpatterns = [
-    url(r'^$', test),
+    url(r'^$', new),
+    url(r'^popular/',popular),
+    url(r'^question/',question)
 ]
