2f7e964d49f41e4ef9616fa1260c34f3649d076b
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-18 11:58:25 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def test (request,*args,**kwargs): 
     return HttpResponse('OK')
+def new (request,*args,**kwargs): 
+    return HttpResponse('OK')
+def popular (request,*args,**kwargs): 
+    return HttpResponse('OK')
+def question (request,*args,**kwargs): 
+    return HttpResponse('OK')
