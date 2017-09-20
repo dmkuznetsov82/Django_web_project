4b59ea411aa915f6a2dbe1adc7a88b3a089521c9
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-20 18:11:30 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^$',include('qa.urls')),
     url(r'^login/',include('qa.urls')),
     url(r'^signup/',include('qa.urls')),
-    url(r'^question/123/',include('qa.urls')),
+    url(r'^question/',include('qa.urls')),
     url(r'^ask/',include('qa.urls')),
     url(r'^popular/',include('qa.urls')),
     url(r'^new/',include('qa.urls')),
