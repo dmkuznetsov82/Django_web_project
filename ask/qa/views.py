538c0ce681ee492f59ba9b32a79ea5a875c79363
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-20 21:25:42 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
-from qa.models import Question
+from qa.models import Question, Answer
 
 def test (request,*args,**kwargs): 
     return HttpResponse('OK')
