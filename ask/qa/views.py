6eddbc2b02536b03d942997e8444af5b1affb299
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-21 11:40:08 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator
+
 from qa.models import Question, Answer
+
 
 def test (request,*args,**kwargs): 
     return HttpResponse('OK')
@@ -9,8 +12,24 @@ def test (request,*args,**kwargs):
 def index (request,*args,**kwargs): 
     return HttpResponse('Index')
 
-def popular (request,*args,**kwargs): 
-    return HttpResponse('Popular')
+def popular (request,page): 
+    try:
+        page = int(request.GET.get("page"))
+    except ValueError:
+        page = 1
+    except TypeError:
+        page = 1
+        
+    questions = Question.object.popular()
+    limit = 10
+    paginator = Paginator(questions,limit)
+    page = paginator.page(page)
+    
+    return render(request, 'popular.html', {
+        'paginator': paginator,
+        'questions': page.object_list,
+        'page': page, })
+    })
 
 @require_GET
 def question_details (request,id): 
