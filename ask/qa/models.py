ab7596808ba57bc87d4cf793e2a1262b0f84575e
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-25 10:25:47 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 class QuestionManager(models.Manager):                                          
-        def new(self):                                                              
-                return self.order_by('-id')                                                          
-        def popular(self):                                                          
-                return self.order_by('-rating')
+    def new(self):                                                              
+        return self.order_by('-id')                                                          
+    def popular(self):                                                          
+        return self.order_by('-rating')
      
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -19,10 +19,12 @@ class Question(models.Model):
     objects = QuestionManager()
 
 class Answer(models.Model):
-  text = models.TextField()
-  added_at = models.DateTimeField()
-  question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-  objects = QuestionManager()
+    objects = QuestionManager()
    
+    def __str__(self):
+        return self.text
