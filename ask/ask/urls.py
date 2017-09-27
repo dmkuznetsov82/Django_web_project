8d660594b8050822e9794b19d6a3bf4da49cc8ce
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-09-27 12:13:22 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from qa.views import index, popular, test
+from qa.views import index, popular, test, ask_add
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
