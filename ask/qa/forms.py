bc3d90ccecdf7cdada6044e7f7f25dd7e250acad
dmkuznetsov82 <31538457+dmkuznetsov82@users.noreply.github.com>
2017-10-11 09:56:24 +0300

    Update forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,25 +1,74 @@
 from django import forms
 from qa.models import Question, Answer, User
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
 class SignupForm(forms.Form):
-    username = forms.CharField()
-    email = forms.CharField()
-    password = forms.CharField()
+    username = forms.CharField(max_length=100, required=False)
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=False)
     
-    def clean(self): pass
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not username:
+            raise forms.ValidationError('Не задано имя пользователя')
+        try:
+            User.objects.get(username=username)
+            raise forms.ValidationError('Такой пользователь уже существует')
+        except User.DoesNotExist:
+            pass
+    return username
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError('Не указан адрес электронной почты')
+    return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if not password:
+            raise forms.ValidationError('Не указан пароль')
+        self.raw_password = password
+    return make_password(password)
     
     def save(self):
         user = User.(**self.cleaned_data)
         user.save()
         return user
+    
+class LoginForm(forms.Form):
+    username = forms.CharField( max_length=100, required=False)
+    password = forms.CharField(widget=forms.PasswordInput, required=False)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not username:
+            raise forms.ValidationError('Не задано имя пользователя')
+        return username
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if not password:
+            raise forms.ValidationError('Не указан пароль')
+        return password
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise forms.ValidationError('Неверное имя пользователя или пароль1')
+        if not user.check_password(password):
+            raise forms.ValidationError('Неверное имя пользователя или пароль2')
 
 class AskForm(forms.Form):
     title = forms.CharField()
     text = forms.CharField()
     
- #   def __init__(self,**kwargs):
- #       super(AskForm,self).__init__(**kwargs)
-        
     def clean(self): pass
     
     def save(self):
@@ -32,9 +81,6 @@ class AskForm(forms.Form):
 class AnswerForm(forms.Form):
     text = forms.CharField()
     question = forms.IntegerField()
-    
- #   def __init__(self,**kwargs):
- #       super(AnswerForm,self).__init__(**kwargs)
         
     def clean(self): pass
     
